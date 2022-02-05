e11f7aa7adf5bd042a9f7c7d0cdb9eb5f3bfae2b
Njoro410 <brian.njoroge@student.moringaschool.com>
2022-02-05 11:04:45 +0300

    added a quote display component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormInputComponent } from './form-input/form-input.component';
+import { QuoteDisplayComponent } from './quote-display/quote-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormInputComponent
+    FormInputComponent,
+    QuoteDisplayComponent
   ],
   imports: [
     BrowserModule,
